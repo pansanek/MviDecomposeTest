2ecb51914c0b886f72c3aa157693fc6f78279910
SashaPotemkin <alex.potjomkin@gmail.com>
2024-08-17 13:56:40 +0300

    ChildInterface

--- app/src/main/java/com/example/mvidecomposetest/ui/content/RootContent.kt
+++ app/src/main/java/com/example/mvidecomposetest/ui/content/RootContent.kt
@@ -21,16 +21,17 @@ fun RootContent(component: DefaultRootComponent) {
                 stack = component.stack
             ) {
                 when (val instance = it.instance) {
-                    is ContactListComponent -> {
-                        Contacts(component = instance)
+
+                    is DefaultRootComponent.Child.AddContact -> {
+                        AddContact(component = instance.component)
                     }
 
-                    is AddContactComponent -> {
-                        AddContact(component = instance)
+                    is DefaultRootComponent.Child.ContactList -> {
+                        Contacts(component = instance.component)
                     }
 
-                    is EditContactComponent -> {
-                        EditContact(component = instance)
+                    is DefaultRootComponent.Child.EditContact -> {
+                        EditContact(component = instance.component)
                     }
                 }
             }
