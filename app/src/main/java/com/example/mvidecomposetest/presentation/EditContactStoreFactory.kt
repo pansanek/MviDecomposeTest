d5de6ea7c7deb001f66d6c645bf16b0c0d6f44bd
SashaPotemkin <alex.potjomkin@gmail.com>
2024-08-18 19:32:21 +0300

    bootstraper

--- app/src/main/java/com/example/mvidecomposetest/presentation/EditContactStoreFactory.kt
+++ app/src/main/java/com/example/mvidecomposetest/presentation/EditContactStoreFactory.kt
@@ -24,7 +24,7 @@ class EditContactStoreFactory(
             ),
             reducer = ReducerImpl,
             executorFactory = ::ExecutorImpl
-        )
+        ) {}
 
     private sealed interface Action
 
